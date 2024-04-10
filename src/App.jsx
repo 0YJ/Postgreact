230a6f60c6d2c742d3c9d99b572097e5d1553c19
YJ <anderskolby@hotmail.com>
2024-04-10 08:50:39 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="logo512.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Guide J <span className="heart">♥️</span> React Postgresql Combo
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+          Build up frontend by checking <code>src</code> and connect to Postgresql.
         </p>
         <p>
           <a
